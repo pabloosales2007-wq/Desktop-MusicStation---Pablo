afe00f51b470bdd61d5ea13091f49d41da8c6d1a
pablosales07 <pabloosales2007@gmail.com>
2026-04-02 17:16:55 -0300

    Apresentação

--- FormFinanceiro.Designer.cs
+++ FormFinanceiro.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.panelSide = new System.Windows.Forms.Panel();
+            this.pictureBoxMenu = new System.Windows.Forms.PictureBox();
+            this.btnFormaPagamento = new System.Windows.Forms.Button();
+            this.pictureBoxLogout = new System.Windows.Forms.PictureBox();
+            this.btnTransacaoesPagamentos = new System.Windows.Forms.Button();
+            this.btnPagamentos = new System.Windows.Forms.Button();
+            this.pboxLogo = new System.Windows.Forms.PictureBox();
             this.panelTop = new System.Windows.Forms.Panel();
             this.panelConteudo = new System.Windows.Forms.Panel();
-            this.btnPagamentos = new System.Windows.Forms.Button();
-            this.btnTransacaoesPagamentos = new System.Windows.Forms.Button();
-            this.btnFormaPagamento = new System.Windows.Forms.Button();
-            this.pboxLogo = new System.Windows.Forms.PictureBox();
-            this.pictureBoxMenu = new System.Windows.Forms.PictureBox();
-            this.pictureBoxLogout = new System.Windows.Forms.PictureBox();
             this.panelSide.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMenu)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // panelSide
@@ -57,6 +57,85 @@
             this.panelSide.Name = "panelSide";
             this.panelSide.Size = new System.Drawing.Size(200, 492);
             this.panelSide.TabIndex = 0;
+            // 
+            // pictureBoxMenu
+            // 
+            this.pictureBoxMenu.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.menuprincipaç;
+            this.pictureBoxMenu.Location = new System.Drawing.Point(156, 449);
+            this.pictureBoxMenu.Name = "pictureBoxMenu";
+            this.pictureBoxMenu.Size = new System.Drawing.Size(35, 31);
+            this.pictureBoxMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMenu.TabIndex = 11;
+            this.pictureBoxMenu.TabStop = false;
+            this.pictureBoxMenu.Click += new System.EventHandler(this.pictureBoxMenu_Click);
+            // 
+            // btnFormaPagamento
+            // 
+            this.btnFormaPagamento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
+            this.btnFormaPagamento.FlatAppearance.BorderSize = 0;
+            this.btnFormaPagamento.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
+            this.btnFormaPagamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFormaPagamento.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormaPagamento.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
+            this.btnFormaPagamento.Location = new System.Drawing.Point(12, 181);
+            this.btnFormaPagamento.Name = "btnFormaPagamento";
+            this.btnFormaPagamento.Size = new System.Drawing.Size(179, 32);
+            this.btnFormaPagamento.TabIndex = 5;
+            this.btnFormaPagamento.Text = "Formas Pagamento";
+            this.btnFormaPagamento.UseVisualStyleBackColor = false;
+            this.btnFormaPagamento.Click += new System.EventHandler(this.btnFormaPagamento_Click);
+            // 
+            // pictureBoxLogout
+            // 
+            this.pictureBoxLogout.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.Logout;
+            this.pictureBoxLogout.Location = new System.Drawing.Point(12, 449);
+            this.pictureBoxLogout.Name = "pictureBoxLogout";
+            this.pictureBoxLogout.Size = new System.Drawing.Size(35, 31);
+            this.pictureBoxLogout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogout.TabIndex = 10;
+            this.pictureBoxLogout.TabStop = false;
+            // 
+            // btnTransacaoesPagamentos
+            // 
+            this.btnTransacaoesPagamentos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
+            this.btnTransacaoesPagamentos.FlatAppearance.BorderSize = 0;
+            this.btnTransacaoesPagamentos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
+            this.btnTransacaoesPagamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTransacaoesPagamentos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTransacaoesPagamentos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
+            this.btnTransacaoesPagamentos.Location = new System.Drawing.Point(12, 143);
+            this.btnTransacaoesPagamentos.Name = "btnTransacaoesPagamentos";
+            this.btnTransacaoesPagamentos.Size = new System.Drawing.Size(179, 32);
+            this.btnTransacaoesPagamentos.TabIndex = 4;
+            this.btnTransacaoesPagamentos.Text = "Transações Pagamentos\r\n";
+            this.btnTransacaoesPagamentos.UseVisualStyleBackColor = false;
+            this.btnTransacaoesPagamentos.Click += new System.EventHandler(this.btnTransacaoesPagamentos_Click);
+            // 
+            // btnPagamentos
+            // 
+            this.btnPagamentos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
+            this.btnPagamentos.FlatAppearance.BorderSize = 0;
+            this.btnPagamentos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
+            this.btnPagamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPagamentos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagamentos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
+            this.btnPagamentos.Location = new System.Drawing.Point(12, 105);
+            this.btnPagamentos.Name = "btnPagamentos";
+            this.btnPagamentos.Size = new System.Drawing.Size(179, 32);
+            this.btnPagamentos.TabIndex = 3;
+            this.btnPagamentos.Text = "Pagamentos";
+            this.btnPagamentos.UseVisualStyleBackColor = false;
+            this.btnPagamentos.Click += new System.EventHandler(this.btnPagamentos_Click);
+            // 
+            // pboxLogo
+            // 
+            this.pboxLogo.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.music_station_vetorizada_1;
+            this.pboxLogo.Location = new System.Drawing.Point(12, 12);
+            this.pboxLogo.Name = "pboxLogo";
+            this.pboxLogo.Size = new System.Drawing.Size(179, 87);
+            this.pboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pboxLogo.TabIndex = 2;
+            this.pboxLogo.TabStop = false;
             // 
             // panelTop
             // 
@@ -75,85 +154,6 @@
             this.panelConteudo.Size = new System.Drawing.Size(668, 444);
             this.panelConteudo.TabIndex = 2;
             // 
-            // btnPagamentos
-            // 
-            this.btnPagamentos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
-            this.btnPagamentos.FlatAppearance.BorderSize = 0;
-            this.btnPagamentos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
-            this.btnPagamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPagamentos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPagamentos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
-            this.btnPagamentos.Location = new System.Drawing.Point(12, 105);
-            this.btnPagamentos.Name = "btnPagamentos";
-            this.btnPagamentos.Size = new System.Drawing.Size(179, 32);
-            this.btnPagamentos.TabIndex = 3;
-            this.btnPagamentos.Text = "Pagamentos";
-            this.btnPagamentos.UseVisualStyleBackColor = false;
-            this.btnPagamentos.Click += new System.EventHandler(this.btnPagamentos_Click);
-            // 
-            // btnTransacaoesPagamentos
-            // 
-            this.btnTransacaoesPagamentos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
-            this.btnTransacaoesPagamentos.FlatAppearance.BorderSize = 0;
-            this.btnTransacaoesPagamentos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
-            this.btnTransacaoesPagamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTransacaoesPagamentos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTransacaoesPagamentos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
-            this.btnTransacaoesPagamentos.Location = new System.Drawing.Point(12, 143);
-            this.btnTransacaoesPagamentos.Name = "btnTransacaoesPagamentos";
-            this.btnTransacaoesPagamentos.Size = new System.Drawing.Size(179, 32);
-            this.btnTransacaoesPagamentos.TabIndex = 4;
-            this.btnTransacaoesPagamentos.Text = "Transações Pagamentos\r\n";
-            this.btnTransacaoesPagamentos.UseVisualStyleBackColor = false;
-            this.btnTransacaoesPagamentos.Click += new System.EventHandler(this.btnTransacaoesPagamentos_Click);
-            // 
-            // btnFormaPagamento
-            // 
-            this.btnFormaPagamento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(30)))), ((int)(((byte)(98)))));
-            this.btnFormaPagamento.FlatAppearance.BorderSize = 0;
-            this.btnFormaPagamento.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(13)))), ((int)(((byte)(101)))));
-            this.btnFormaPagamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFormaPagamento.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFormaPagamento.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(180)))), ((int)(((byte)(34)))));
-            this.btnFormaPagamento.Location = new System.Drawing.Point(12, 181);
-            this.btnFormaPagamento.Name = "btnFormaPagamento";
-            this.btnFormaPagamento.Size = new System.Drawing.Size(179, 32);
-            this.btnFormaPagamento.TabIndex = 5;
-            this.btnFormaPagamento.Text = "Formas Pagamento";
-            this.btnFormaPagamento.UseVisualStyleBackColor = false;
-            this.btnFormaPagamento.Click += new System.EventHandler(this.btnFormaPagamento_Click);
-            // 
-            // pboxLogo
-            // 
-            this.pboxLogo.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.music_station_vetorizada_1;
-            this.pboxLogo.Location = new System.Drawing.Point(12, 12);
-            this.pboxLogo.Name = "pboxLogo";
-            this.pboxLogo.Size = new System.Drawing.Size(179, 87);
-            this.pboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pboxLogo.TabIndex = 2;
-            this.pboxLogo.TabStop = false;
-            // 
-            // pictureBoxMenu
-            // 
-            this.pictureBoxMenu.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.menuprincipaç;
-            this.pictureBoxMenu.Location = new System.Drawing.Point(156, 449);
-            this.pictureBoxMenu.Name = "pictureBoxMenu";
-            this.pictureBoxMenu.Size = new System.Drawing.Size(35, 31);
-            this.pictureBoxMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxMenu.TabIndex = 11;
-            this.pictureBoxMenu.TabStop = false;
-            this.pictureBoxMenu.Click += new System.EventHandler(this.pictureBoxMenu_Click);
-            // 
-            // pictureBoxLogout
-            // 
-            this.pictureBoxLogout.Image = global::Telas_Desktop_MusicStation___Pablo.Properties.Resources.Logout;
-            this.pictureBoxLogout.Location = new System.Drawing.Point(12, 449);
-            this.pictureBoxLogout.Name = "pictureBoxLogout";
-            this.pictureBoxLogout.Size = new System.Drawing.Size(35, 31);
-            this.pictureBoxLogout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxLogout.TabIndex = 10;
-            this.pictureBoxLogout.TabStop = false;
-            // 
             // FormFinanceiro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -166,9 +166,9 @@
             this.Name = "FormFinanceiro";
             this.Text = "FormFinanceiro";
             this.panelSide.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMenu)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
